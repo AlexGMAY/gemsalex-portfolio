7bfa06b57049c4376ac33e3b56fa8441e040e6dc
Alex <gemstone.merdy@gmail.com>
2025-02-12 14:59:45 +0100

    Layout - Footer fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Merveille Alexander",
+  description: "FullStack Software Engineer, WordPress Developer and Auto-entrepreneur",
 };
 
 export default function RootLayout({
